0215cb497489bcf5b8f778e4260e52246420af49
juhairulislam <juhairulislam2018@gmail.com>
2026-03-13 06:35:39 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+
+function App() {
+
+  function handleClick(){
+    alert('I am clicked')
+  }
+
+  const handleClick2 = ()=>{
+    alert('clicked two')
+  }
+
+  // with parameter ;
+  const handleAdd5 = (num)=>{
+    const newNum = num +5 ;
+    alert(newNum)
+  }
+
+  return (
+    <>
+    <h3>Vite + react</h3>
+    <button onClick={handleClick}>Click Me</button>
+    <button onClick={handleClick2}>Click Me</button>
+    <button onClick={()=>handleAdd5(10)}>Click 5</button>
+    </>
+  )
+}
+
+export default App
